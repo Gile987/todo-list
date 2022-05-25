089a81cf19bd928f7281527f519e8b319d4e6b07
Mladen Reljic <mladen.reljic@noubis.com>
2022-05-25 14:27:31 +0200

    Add a function for list item editing - WIP

--- app.js
+++ app.js
@@ -22,14 +22,30 @@ const handleItem = (itemName) => {
           return item !== itemName;
         });
       });
+
+      item.querySelector('.edit-item').addEventListener('click', () => {
+        itemInput.value = itemName;
+        itemList.removeChild(item);
+        todoItems = todoItems.filter(function (item) {
+          return item !== itemName;
+        });
+      });
     };
   });
+};
+
+const setLocalStorage = todoItems => {
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
 };
 
 const getList = todoItems => {
   itemList.innerHTML = '';
   todoItems.forEach(item => {
-    itemList.insertAdjacentHTML('beforeend', `<div class="item"><h5 class="item-name">${item}</h5><div class="item-icons"><a href="#" class="complete-item item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
+    itemList.insertAdjacentHTML('beforeend', `<div class="item"><h5 class="item-name">${item}</h5>
+      <div class="item-icons"><a href="#" class="complete-item item-icon"><i class="far fa-check-circle"></i>
+      </a><a href="#" class="edit-item item-icon"><i class="far fa-edit"></i></a>
+      <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`
+    );
     handleItem(item);
   });
 };
@@ -42,11 +58,12 @@ form.addEventListener('submit', e => {
   if (itemName.length === 0) {
     alert.innerHTML = 'You can\'t leave the field empty';
     alert.classList.add('display-alert', 'alert');
-    setTimeout( () =>{
+    setTimeout(() => {
       alert.classList.remove('display-alert');
     }, 3000);
   } else {
     todoItems.push(itemName);
+    setLocalStorage(todoItems);
     getList(todoItems);
   };
   itemInput.value = '';
@@ -54,5 +71,6 @@ form.addEventListener('submit', e => {
 
 clearList.addEventListener('click', () => {
   todoItems = [];
+  localStorage.clear();
   getList(todoItems);
-})
+});
