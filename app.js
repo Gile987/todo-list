082a0068e0b95e7f2023c262b2afcec9c0a307b8
Mladen Reljic <mladen.reljic@noubis.com>
2022-05-26 11:55:07 +0200

    Write script that returns items saved into local storage, write script that deletes items from local storage when they are deleted from the list. Add comments.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// declare constants/variables
 const form = document.querySelector('#form');
 const itemInput = document.querySelector('.item-input');
 const itemList = document.querySelector('.item-list');
@@ -11,22 +12,25 @@ const handleItem = (itemName) => {
 
   items.forEach(item => {
     if (item.querySelector('.item-name').textContent === itemName) {
+      // completed item event listener
       item.querySelector('.complete-item').addEventListener('click', () => {
         item.querySelector('.item-name').classList.toggle('completed');
         item.classList.toggle('visibility');
       });
-
+      // delete item from list event listener
       item.querySelector('.delete-item').addEventListener('click', () => {
         itemList.removeChild(item);
         todoItems = todoItems.filter(item => {
           return item !== itemName;
         });
+        // delete item from local storage
+        setLocalStorage(todoItems);
       });
-
+      // edit item event listener
       item.querySelector('.edit-item').addEventListener('click', () => {
         itemInput.value = itemName;
         itemList.removeChild(item);
-        todoItems = todoItems.filter(function (item) {
+        todoItems = todoItems.filter((item) => {
           return item !== itemName;
         });
       });
@@ -38,29 +42,40 @@ const setLocalStorage = todoItems => {
   localStorage.setItem('todoItems', JSON.stringify(todoItems));
 };
 
+const getLocalStorage = () => {
+  const todoStorage = localStorage.getItem('todoItems');
+  if (todoStorage === 'undefined' || todoStorage === null) {
+    todoItems = [];
+  } else {
+    todoItems = JSON.parse(todoStorage);
+    getList(todoItems);
+  };
+};
+
+// render the list items
 const getList = todoItems => {
   itemList.innerHTML = '';
   todoItems.forEach(item => {
+    // use beforeend parameter to insert items after last child
     itemList.insertAdjacentHTML('beforeend', `<div class="item"><h5 class="item-name">${item}</h5>
       <div class="item-icons"><a href="#" class="complete-item item-icon"><i class="far fa-check-circle"></i>
       </a><a href="#" class="edit-item item-icon"><i class="far fa-edit"></i></a>
-      <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`
-    );
+      <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
     handleItem(item);
   });
 };
 
-
 form.addEventListener('submit', e => {
   e.preventDefault();
   const itemName = itemInput.value;
-
+  // show a warning if no item name has been entered
   if (itemName.length === 0) {
     alert.innerHTML = 'You can\'t leave the field empty';
     alert.classList.add('display-alert', 'alert');
     setTimeout(() => {
       alert.classList.remove('display-alert');
     }, 3000);
+    // push the item name into the array & save into local storage  
   } else {
     todoItems.push(itemName);
     setLocalStorage(todoItems);
@@ -69,8 +84,11 @@ form.addEventListener('submit', e => {
   itemInput.value = '';
 });
 
+// empty the todoItems array and clear local storage when clicking on the "clear items" button
 clearList.addEventListener('click', () => {
   todoItems = [];
   localStorage.clear();
   getList(todoItems);
 });
+
+getLocalStorage();
